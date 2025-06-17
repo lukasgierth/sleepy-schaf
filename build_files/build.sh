ab3c6c9702fd1139607ac17b59a6cf149882ad3b
Lukas Gierth <lukas.gierth@posteo.de>
2025-06-17 16:34:22 +0200

    feat: remove more kde stuff

--- build_files/build.sh
+++ build_files/build.sh
@@ -20,6 +20,25 @@ dnf5 -y remove vim
 # KDE specific
 dnf5 -y remove plasma-discover
 dnf5 -y remove plasma-welcome
+dnf5 -y remove kate
+dnf5 -y remove kwrite
+dnf5 -y remove plasma-emojier
+dnf5 -y remove filelight
+dnf5 -y remove khelpcenter
+dnf5 -y remove firewall-config
+dnf5 -y remove kjournald
+dnf5 -y remove kcharselect
+# needed? test
+dnf5 -y remove fcitx5-qt
+dnf5 -y remove fcitx5-gtk
+dnf5 -y remove fcitx5-chinese-addons
+dnf5 -y remove fcitx5-hangul
+dnf5 -y remove fcitx5-libthai
+dnf5 -y remove fcitx5-mozc
+dnf5 -y remove fcitx5-sayura
+dnf5 -y remove fcitx5-unikey
+dnf5 -y remove fcitx5-configtool
+dnf5 -y remove kcm-fcitx5
 
 # INSTALL
 #
