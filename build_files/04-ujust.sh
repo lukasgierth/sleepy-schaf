ac9c0f49b9207e8442581bad1c60372539c454b1
Lukas Gierth <lukas.gierth@posteo.de>
2025-06-18 17:56:12 +0200

    fix: forgot to cp files

--- build_files/04-ujust.sh
+++ build_files/04-ujust.sh
@@ -6,6 +6,8 @@ set -ouex pipefail
 rm -f /usr/share/ublue-os/just/*.just
 
 # create own file
+cp -f /ctx/files/97-mise.just /usr/share/ublue-os/just/97-mise.just
+cp -f /ctx/files/98-flatpak.just /usr/share/ublue-os/just/98-flatpak.just
 cp -f /ctx/files/99-custom.just /usr/share/ublue-os/just/99-custom.just
 cp -f /ctx/files/distrobox.ini /etc/distrobox/distrobox.ini
 cp -f /ctx/files/toolbox.ini /etc/toolbox/toolbox.ini
