f67ed8d1051709634f6ec913e2ce2e4cd7661a03
Lukas Gierth <lukas.gierth@posteo.de>
2025-06-18 15:55:06 +0200

    chore: rework

---
+++ build_files/01-remove-packages.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+set -ouex pipefail
+
+# no need for them
+dnf5 -y remove firefox
+dnf5 -y remove firefox-langpacks
+dnf5 -y remove htop
+dnf5 -y remove net-tools
+dnf5 -y remove vim
+
+# GNOME specific
+dnf5 -y remove gnome-classic-session
+dnf5 -y remove gnome-shell-extension-apps-menu
+dnf5 -y remove gnome-shell-extension-background-logo
+dnf5 -y remove gnome-shell-extension-launch-new-instance
+dnf5 -y remove gnome-shell-extension-places-menu
+dnf5 -y remove gnome-shell-extension-window-list
+dnf5 -y remove gnome-software
+dnf5 -y remove gnome-tour
+dnf5 -y remove gnome-tweaks
+dnf5 -y remove yelp
+
+# GNOME specific
+dnf5 -y install gnome-shell-extension-caffeine
