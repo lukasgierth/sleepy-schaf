a07f0f17a436e04d557d00f66bc08ccfb4c4901a
Lukas Gierth <lukas.gierth@posteo.de>
2025-06-18 16:02:45 +0200

    fix: path; and cleanup

--- build_files/04-ujust.sh
+++ build_files/04-ujust.sh
@@ -8,7 +8,7 @@ rm -f /usr/share/ublue-os/just/*.just
 # create own file
 cp -f /ctx/files/99-custom.just /usr/share/ublue-os/just/99-custom.just
 cp -f /ctx/files/distrobox.ini /etc/distrobox/distrobox.ini
-cp -f /ctx/filestoolbox.ini /etc/toolbox/toolbox.ini
+cp -f /ctx/files/toolbox.ini /etc/toolbox/toolbox.ini
 
 # cleanup justfile imports
 sed -i '/^import/d' /usr/share/ublue-os/justfile
